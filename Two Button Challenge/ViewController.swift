717ff586236027c76f7ccd46aac7560b822c0c35
bycloud345 <byoung345@icloud.com>
2023-02-08 09:42:38 -0700

    Completed Two Button challenge with text color and text alignment

--- Two Button Challenge/ViewController.swift
+++ Two Button Challenge/ViewController.swift
@@ -17,11 +17,13 @@ class ViewController: UIViewController {
     @IBAction func showMessagePressed(_ sender: UIButton) {
         messageLabel.text = "You Are Awesome!"
         messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = .left
     }
     
     @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
         messageLabel.text = "You Are Great!"
         messageLabel.textColor = .systemRed
+        messageLabel.textAlignment = .right
 
     }
     
